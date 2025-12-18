a927228069a3636a77a0eec3121712ffdbe63d72
user2982024 <sheikhabrar217@gmail.com>
2025-12-18 11:06:05 +0500

    Database connected!

---
+++ Backend/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { type } = require("os");
+const { resourceUsage } = require("process");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true, // removes extra spaces
+      minLength: 4,
+      maxLength: 50,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email address",
+      ],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: 6,
+      select: false, // never return password in queries
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./config/db');
 
 dotenv.config();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Connect Database
+connectDB();
+
+// Test Route
 app.get('/', (req, res) => {
     res.status(200).json({
         success: true,
