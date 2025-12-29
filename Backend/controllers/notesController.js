f0d8b063f5f3cffd57323e0be82834b1d482b258
user2982024 <sheikhabrar217@gmail.com>
2025-12-29 19:46:29 +0500

    Some updated in controller!

--- Backend/controllers/notesController.js
+++ Backend/controllers/notesController.js
@@ -98,7 +98,10 @@ exports.editNote = async (req, res) => {
 // Get note by id (GET)
 exports.getNoteById = async (req, res) => {
   try {
-    const note = await Note.findById(req.params.id);
+    const note = await Note.findOne({
+      _id: req.params.id,
+      user: req.user._id
+    })
 
     if (!note) {
       return res.status(404).json({
