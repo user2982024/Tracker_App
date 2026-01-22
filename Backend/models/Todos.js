9cf1bd3570e56a449f4071d04a5f72f0e012300b
user2982024 <sheikhabrar217@gmail.com>
2026-01-22 17:23:13 +0500

    Todos model created!

---
+++ Backend/models/Todos.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: [true, "Todo title is required"],
+      trim: true,
+      maxlength: [100, "Title cannot exceed 100 characters"],
+    },
+
+    description: {
+      type: String,
+      trim: true,
+      maxlength: [500, "Description cannot exceed 500 characters"],
+    },
+
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+
+    priority: {
+        type: String,
+        enum: ["low", "medium", "high"],
+        default: "medium"
+    },
+
+    dueDate: {
+      type: Date,
+    },
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true,
+    },
+}, 
+{
+    timestamps: true,
+}
+);
+
+module.exports = mongoose.model("Todo", todoSchema);
