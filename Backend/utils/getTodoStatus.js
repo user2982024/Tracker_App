c33c69d442381d26bdc1409552db25d0219b059c
user2982024 <sheikhabrar217@gmail.com>
2026-03-05 07:57:33 +0500

    Bug solved!

--- Backend/utils/getTodoStatus.js
+++ Backend/utils/getTodoStatus.js
@@ -1,15 +1,14 @@
 const getTodoStatus = (todo) => {
-    const now = new Dtae();
+  const now = new Dtae();
 
-    if (todo.completed) {
-        return "Completed";
-    }
-
-    if (todo.dueDate && new Date(todo.dueDate) < now) {
-    return "overdue";
-
-    return "in progress";
+  if (todo.completed) {
+    return "Completed";
   }
+
+  if (todo.dueDate && new Date(todo.dueDate) < now) {
+    return "overdue";
+  }
+  return "in progress";
 };
 
 module.exports = getTodoStatus;
