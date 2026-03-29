fd1343406d94b47e155eb856542db3c48a3c132f
user2982024 <sheikhabrar217@gmail.com>
2026-03-29 11:04:59 +0500

    Input validation implemented!

---
+++ Backend/validators/authValidator.js
@@ -0,0 +1,22 @@
+const { body } = require("express-validator");
+
+exports.signupValidator = [
+    body("name")
+    .trim()
+    .notEmpty().withMessage("Name is required")
+    .isLength({ min: 5, max: 30 }).withMessage("Name must be between 5 and 30 characters"),
+
+    body("email")
+    .trim()
+    .notEmpty().withMessage("Email is required")
+    .isEmail().withMessage("Please provide a valid email")
+    .normalizeEmail(),
+
+    body("password")
+    .trim()
+    .notEmpty().withMessage("Password is required")
+    .isLength({ min: 6 }).withMessage("Password must be atleast 6 characters")
+    .matches()
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/)
+    .withMessage("Password must contain uppercase, lowercase and a number")
+];
